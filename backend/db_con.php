c2022cc0f6b173dd0f641855d705b8c0081c4196
Kopal <kopaljain03@gmail.com>
2021-10-23 19:00:23 +0530

    login and index backend

---
+++ backend/db_con.php
@@ -0,0 +1,14 @@
+<?php
+$servername="localhost";
+$username="root";
+$password="";
+$database="smp_website";
+
+$con=mysqli_connect($servername,$username,$password,$database);
+
+if(!$con){
+die("nope no conn bruh");
+}
+else
+echo "conn established nyaaaa";
+?>
